507a6d55f40ecf07d73ee73f930e004bec80248c
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-12-02 15:00:38 -0300

    Adiciona funcionalidade aos botoes

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,28 +1,55 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function NavBar() {
+  const history = useHistory();
   const { name, role } = JSON.parse(localStorage.getItem('user'));
+
+  const goTo = (endpoint) => {
+    history.push(endpoint);
+  };
+
   const roleButton = {
     administrator:
-  <button data-testid="customer_products__element-navbar-link-orders" type="button">
+  <button
+    data-testid="customer_products__element-navbar-link-orders"
+    type="button"
+    onClick={ () => goTo('/administrator/management') }
+  >
     GERENCIAR USUÁRIOS
   </button>,
     seller:
-  <button data-testid="customer_products__element-navbar-link-orders" type="button">
+  <button
+    data-testid="customer_products__element-navbar-link-orders"
+    type="button"
+    onClick={ () => goTo('/seller/orders') }
+  >
     PEDIDOS
   </button>,
     customer:
   <span>
-    <button data-testid="customer_products__element-navbar-link-products" type="button">
+    <button
+      data-testid="customer_products__element-navbar-link-products"
+      type="button"
+      onClick={ () => goTo('/customer/products') }
+    >
       PRODUTOS
     </button>
-    <button data-testid="customer_products__element-navbar-link-orders" type="button">
+    <button
+      data-testid="customer_products__element-navbar-link-orders"
+      type="button"
+      onClick={ () => goTo('/customer/orders') }
+    >
       MEUS PEDIDOS
     </button>
   </span>,
   };
 
-  console.log(role);
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    goTo('/login');
+  };
 
   return (
     <header>
@@ -31,7 +58,11 @@ function NavBar() {
         <span data-testid="customer_products__element-navbar-user-full-name">
           { name }
         </span>
-        <button data-testid="customer_products__element-navbar-link-logout" type="button">
+        <button
+          data-testid="customer_products__element-navbar-link-logout"
+          type="button"
+          onClick={ logout }
+        >
           Sair
         </button>
       </nav>
