91fd6c054b2993eab1ebf60d34a73c7d32486150
Augusto Varandas <augustovarandas21@gmail.com>
2022-12-08 18:03:42 -0300

    Consulta arquivo de secret_key

--- back-end/src/auth/JWT.js
+++ back-end/src/auth/JWT.js
@@ -1,20 +1,29 @@
 const jwt = require('jsonwebtoken');
 const CustomError = require('../utils/CustomError');
+const fs = require('fs/promises');
 
-const generateToken = (user) => {
+const filePath = __dirname + '/../../jwt.evaluation.key'; 
+
+const getSecretKey = async () => await fs.readFile(filePath, { encoding: 'utf8' });
+
+const generateToken = async (user) => {
   const payload = {
     id: user.id,
     role: user.role,
   };
 
-  const token = jwt.sign(payload, 'secret_key');
+  const secretKey = await getSecretKey();
+
+  const token = jwt.sign(payload, secretKey);
 
   return token;
 };
 
-const authenticate = (token) => {
+const authenticate = async (token) => {
+  const secretKey = await getSecretKey();
+
   try {
-    const payload = jwt.verify(token, 'secret_key');
+    const payload = jwt.verify(token, secretKey);
     return payload;
   } catch (error) {
     throw new CustomError('Invalid token', 401);
