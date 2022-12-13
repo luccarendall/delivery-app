e716eb03bdfba91f5339ba8e0811360006678f8b
LuizFelipe406 <luizfpereira406@gmail.com>
2022-12-13 18:10:36 -0300

    Fix Lint

--- back-end/src/controllers/imageController.js
+++ back-end/src/controllers/imageController.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const getImages = (req, res) => {
   const { name } = req.params;
-  const pathName = path.join(__dirname, `/../images/${name}`)
+  const pathName = path.join(__dirname, `/../images/${name}`);
   return res.status(200).sendFile(pathName);
-}
+};
 
-module.exports = { getImages }
+module.exports = { getImages };
