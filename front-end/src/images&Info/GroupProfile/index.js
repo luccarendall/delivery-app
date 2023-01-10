e2585c2876fd924a780416cde5bfbf6d2ad163ec
Henriqueprim <henrique_salmprim@hotmail.com>
2023-01-10 19:02:05 -0300

    doc: adds devs profile pictures creates an index

---
+++ front-end/src/images&Info/GroupProfile/index.js
@@ -0,0 +1,13 @@
+import Augusto from './AugustoVarandas.jpeg';
+import Cristiane from './CristianeDutra.jpeg';
+import Gabriel from './GabrielGomes.jpeg';
+import Henrique from './HenriquePrim.jpg';
+import Lucca from './LuccaRendall.jpeg';
+import Luiz from './LuizFelipe.png';
+
+export const AugustoVarandas = Augusto;
+export const CristianeDutra = Cristiane;
+export const GabrielGomes = Gabriel;
+export const HenriquePrim = Henrique;
+export const LuccaRendall = Lucca;
+export const LuizFelipe = Luiz;
