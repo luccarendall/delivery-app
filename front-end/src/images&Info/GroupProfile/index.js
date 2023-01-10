a05a25ada059c91812e4c36efdb62a2d51926250
Henriqueprim <henrique_salmprim@hotmail.com>
2023-01-10 19:18:18 -0300

    doc: fixes img paths and other imgs changes

--- front-end/src/images&Info/GroupProfile/index.js
+++ front-end/src/images&Info/GroupProfile/index.js
@@ -1,12 +1,10 @@
 import Augusto from './AugustoVarandas.jpeg';
-import Cristiane from './CristianeDutra.jpeg';
 import Gabriel from './GabrielGomes.jpeg';
 import Henrique from './HenriquePrim.jpg';
 import Lucca from './LuccaRendall.jpeg';
 import Luiz from './LuizFelipe.png';
 
 export const AugustoVarandas = Augusto;
-export const CristianeDutra = Cristiane;
 export const GabrielGomes = Gabriel;
 export const HenriquePrim = Henrique;
 export const LuccaRendall = Lucca;
