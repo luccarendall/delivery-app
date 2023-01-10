bc8718c675c2a1eb3d0ca18e39ca50cf6c9bf18d
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2023-01-10 19:07:24 -0300

    Estiliza página de admin

--- front-end/src/pages/admin/Management.js
+++ front-end/src/pages/admin/Management.js
@@ -76,16 +76,28 @@ function Management() {
   const invalidRegisterMessage = (
     <span
       data-testid="admin_manage__element-invalid-register"
+      className="text-red-500 font-bold"
     >
       Dados inválidos
     </span>);
 
+  const classInput = `appearance-none p-2 border-b rounded border-grey focus:outline-none
+  focus:border-yellow focus:border-b-2 bg-white`;
+
+  const classLabel = `mb-2 text-sm font-medium text-gray-900 dark:text-white mr-4 flex
+  flex-col justify-center items-start`;
+
+  const classButton = `shadow bg-yellow hover:bg-h-yellow text-gray-800 font-bold p-2
+  rounded disabled:bg-grey duration-200`;
+
   return (
     <div>
       <NavBar />
-      <p>Cadastrar novo usuário</p>
-      <form>
-        <label htmlFor="name-input-admin-manage">
+      <p className="text-1xl font-medium w-[1037px] mx-auto mt-8">
+        Cadastrar novo usuário
+      </p>
+      <form className="w-max mx-auto bg-white p-2 rounded shadow flex mt-2">
+        <label htmlFor="name-input-admin-manage" className={ classLabel }>
           Nome
           <input
             onChange={ handleNameChange }
@@ -94,9 +106,10 @@ function Management() {
             type="name"
             placeholder="Nome e sobrenome"
             value={ name }
+            className={ classInput }
           />
         </label>
-        <label htmlFor="email-input-admin-manage">
+        <label htmlFor="email-input-admin-manage" className={ classLabel }>
           Email
           <input
             onChange={ handleEmailChange }
@@ -105,9 +118,10 @@ function Management() {
             type="email"
             placeholder="seu-email@site.com.br"
             value={ email }
+            className={ classInput }
           />
         </label>
-        <label htmlFor="password-input-admin-manage">
+        <label htmlFor="password-input-admin-manage" className={ classLabel }>
           Senha
           <input
             onChange={ handlePasswordChange }
@@ -116,31 +130,36 @@ function Management() {
             type="password"
             placeholder="**********"
             value={ password }
+            className={ classInput }
           />
         </label>
-        <label htmlFor="role-select-admin-manage">
+        <label htmlFor="role-select-admin-manage" className={ classLabel }>
           Tipo
           <select
             onChange={ handleRoleChange }
             data-testid="admin_manage__select-role"
             id="role-select-admin-manage"
             value={ role }
+            className={ classInput }
           >
             <option defaultValue="customer" value="customer">Cliente</option>
             <option value="seller">Vendedor</option>
             <option value="administrator">Administrador</option>
           </select>
         </label>
-        { badRegister && invalidRegisterMessage }
         <button
           onClick={ register }
           type="button"
           data-testid="admin_manage__button-register"
           disabled={ isDisabled }
+          className={ classButton }
         >
           Cadastrar
         </button>
       </form>
+      <div className="text-center mt-2">
+        { badRegister && invalidRegisterMessage }
+      </div>
     </div>
   );
 }
